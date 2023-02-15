1de301bb7df820c7a6725dc3721367af1e47fc1f
Wener Rodrigo <wener19.wr@gmail.com>
2023-02-15 19:58:35 -0300

    projeto finalizado

--- src/components/UltimosLancamentos/index.js
+++ src/components/UltimosLancamentos/index.js
@@ -1,37 +1,43 @@
-import { livros } from './dadosUltimosLancamentos';
-import styled from 'styled-components';
-import { Titulo } from '../Titulo'
+import { livros } from "./dadosUltimosLancamentos";
+import styled from "styled-components";
+import { Titulo } from "../Titulo";
+import CardRecomenda from "../CardRecomenda";
+import  imagemLivro  from '../../img/livro2.png';
 
 const UltimosLancamentosContainer = styled.section`
-    background-color: #EBECEE;
-    padding-bottom: 20px;
-    display: flex;
-    flex-direction: column;
-`
+  background-color: #ebecee;
+  padding-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+`;
 
 const NovosLivrosContainer = styled.div`
-    margin-top: 30px;
-    display: flex;
-    width: 100%;
-    justify-content: center;
-    cursor: pointer;
-`
-
+  margin-top: 30px;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  cursor: pointer;
+`;
 
 function UltimosLancamentos() {
-    return (
-        <UltimosLancamentosContainer>
-            <Titulo cor="#EB9B00" 
-            tamanhoFonte="36px" 
-            >Latest releases</Titulo>
-            <NovosLivrosContainer>
-            {livros.map(livro => (
-                <img src={livro.src}/>
-            ))}
-            </NovosLivrosContainer>
-        </UltimosLancamentosContainer>
-        
-    )
+  return (
+    <UltimosLancamentosContainer>
+      <Titulo cor="#EB9B00" tamanhoFonte="36px">
+        Latest releases
+      </Titulo>
+      <NovosLivrosContainer>
+        {livros.map((livro) => (
+          <img src={livro.src} />
+        ))}
+      </NovosLivrosContainer>
+      <CardRecomenda
+        titulo="Perhaps you are interested in.."
+        subtitulo="Angular 11"
+        descricao="Building an application with the Google platform."
+        img={imagemLivro}
+      />
+    </UltimosLancamentosContainer>
+  );
 }
 
 export default UltimosLancamentos;
